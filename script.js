6a090dd248d374868f88f117994620910b9fbc26
Mugdi-agence <contact.mugdi@gmail.com>
2025-05-31 11:54:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
-
+import * as THREE from 'https://esm.sh/three@0.158.0';
+import { GLTFLoader } from 'https://esm.sh/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
 
 const loader = new GLTFLoader();
 
